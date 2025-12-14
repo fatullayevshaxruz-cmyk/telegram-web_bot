377fd44710900fbb9308f8399dbf2c26da1e0a2c
fatullayevshaxruz-cmyk <fatullayevshaxruz@gmail.com>
2025-12-14 20:05:38 +0500

    daw

--- src/App.jsx
+++ src/App.jsx
@@ -64,10 +64,12 @@ const App = () => {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(cartItems),
+        body: JSON.stringify
+        ({products: cartItems, 
+          queryID: queryID}),
       }) ;
     }else{
-      telegram.sendData(JSON.stringify ({products: cartItems, queryID: queryID})
+      telegram.sendData(JSON.stringify(cartItems)
    )  }
   }, [cartItems]);
   useEffect (() => {
